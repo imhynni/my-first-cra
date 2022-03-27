80a200bd251eecb0ffe129dfa7549ef191b49c5f
imhynni <omelas329@gmail.com>
2022-03-28 02:29:48 +0900

    Coin Tracker Final 입력한 달러로 선택한 코인을 얼마나 살 수 있는지 알려주는 프로그램 select, option, value, state, eventListener 복습

--- src/App.js
+++ src/App.js
@@ -3,25 +3,52 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [dollars, setDollars] = useState(0);
+  const [selectedCoin, setSelectedCoin] = useState(0);
+  const [result, setResult] = useState(null);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers?limit=100")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
         setLoading(false);
+        setSelectedCoin(json[0].quotes.USD.price);
       });
   }, []);
+  const onDollarChange = (event) => {
+    setDollars(event.target.value);
+  };
+  const onSelectChange = (event) => {
+    setSelectedCoin(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setResult(dollars / selectedCoin);
+  };
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li key={coin.id}>
-            {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onSelectChange}>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onDollarChange}
+          value={dollars}
+          type="number"
+          placeholder="how much do you have?"
+        ></input>
+        <button>exchange</button>
+      </form>
+      {result === null ? null : <span>You can get {result} coins</span>}
     </div>
   );
 }
